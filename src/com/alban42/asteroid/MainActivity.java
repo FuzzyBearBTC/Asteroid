ec2ac70960cd6ccbadf8a12e98302e11ad268ca8
Alban Ménager <alban.menager@gmail.com>
2013-03-08 19:00:57 +0100

    OPEN - task 2: Creación de una interfaz de usuario usando XML http://github.com/Alban42/Asteroid/issues/issue/2

--- src/com/alban42/asteroid/MainActivity.java
+++ src/com/alban42/asteroid/MainActivity.java
@@ -10,10 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.activity_main);
-		TextView text = new TextView(this);
-		text.setText("42");
-		setContentView(text);
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
